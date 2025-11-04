5d2421e4837f318dbb5abd598a80705f63af1df8
abdel-hady <iomicrosoft505@gmail.com>
2025-11-05 00:45:36 +0300

    feat: implement authentication with jwt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,10 +3,16 @@ import { Sign } from 'crypto';
 import { SignInInput } from './dto/signin.input';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { verify } from 'argon2';
+import { JwtService } from '@nestjs/jwt';
+import { AuthJwtPayload } from './types/auth-jwtPayload';
+import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwtService: JwtService,
+  ) {}
   async validateLocalUser({ email, password }: SignInInput) {
     const user = await this.prisma.user.findUnique({ where: { email } });
     if (!user) {
@@ -19,5 +25,21 @@ export class AuthService {
     }
 
     return user;
+  }
+
+  async generateJwtToken(userId: number) {
+    const payload: AuthJwtPayload = { sub: userId };
+    const accessToken = await this.jwtService.signAsync(payload);
+    return { accessToken };
+  }
+
+  async login(user: User) {
+    const { accessToken } = await this.generateJwtToken(user.id);
+    return {
+      id: user.id,
+      name: user.name,
+      accessToken,
+      avatar: user.avatar,
+    };
   }
 }
