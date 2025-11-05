26eed412424d1df46f47791cf066eb51e34d9082
abdel-hady <iomicrosoft505@gmail.com>
2025-11-05 11:18:15 +0300

    feat: protect graphql

--- src/post/post.resolver.ts
+++ src/post/post.resolver.ts
@@ -1,13 +1,19 @@
-import { Resolver, Query } from '@nestjs/graphql';
+import { Resolver, Query, Context } from '@nestjs/graphql';
 import { PostService } from './post.service';
 import { Post } from './entities/post.entity';
+import { UseGuards } from '@nestjs/common';
+import { JwtAuthGuard } from 'src/auth/guards/jwt-auth/jwt-auth.guard';
 
 @Resolver(() => Post)
 export class PostResolver {
   constructor(private readonly postService: PostService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Query(() => [Post], { name: 'post' })
-  findAll() {
+  findAll(@Context() context) {
+    const user = context.req.user;
+    console.log(user);
+
     return this.postService.findAll();
   }
 }
